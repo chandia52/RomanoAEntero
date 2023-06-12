429589dc30efcc5b5bb4c27386d97284c23faea7
chandia52 <jorgechandia7@gmail.com>
2023-06-12 01:30:09 -0300

    First Commit

---
+++ RomanoAEntero.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s: str):
+        valores= {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        s= s.replace("IV","IIII").replace("IX","VIIII")
+        s= s.replace("XL","XXXX").replace("XC","LXXXX")
+        s= s.replace("CD","CCCC").replace("CM","DCCCC")
+        numero=0
+        for char in s:
+            numero += valores[char]
+        return numero
+    
+solucion = Solution()
+numero_romano= input("Ingrese el valor en numeros romanos:")
+resultado=solucion.romanToInt(numero_romano)
+print(resultado) 
+
